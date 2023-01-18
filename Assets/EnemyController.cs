fe23432a1e3684ec5d8254e443c9f199ed60f82c
ykoval201 <ykoval201@gmail.com>
2023-01-18 12:15:00 -0600

    1
    Added Collider and made enemy move towards player

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -10,16 +10,47 @@ public class EnemyController : MonoBehaviour
     private int maxHealth = 10; 
     [SerializeField]
     private int currentHealth;
+    private Rigidbody2D rb2D;
+    private float distanceToPlayer;
+    [SerializeField]
+    private PlayerController player;
+    private Vector2 targetPosition;
+
+
+       
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        targetPosition = rb2D.position;
+        
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+            distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
+        
+    }
 
+    void FixedUpdate()
+    {
+        if (IsWithinCamera())
+        {
+            targetPosition = player.transform.position;
+            //move towards player
+            Vector2 newPosition = Vector2.MoveTowards(rb2D.position, targetPosition, movementSpeed);
+            rb2D.MovePosition(newPosition);
+        }
     }
 
     //This function is used to have enemies both take and recieve damage. call change health with negative values to take damage, and positive values to heal damage.
