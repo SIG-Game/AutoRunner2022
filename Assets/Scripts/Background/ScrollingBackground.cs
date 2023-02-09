803112fbafef3114463ec2711a5bd003381d1f26
Isaac Sanchez <isanch35@uic.edu>
2023-02-08 18:49:56 -0600

    First attempt at scrolling background

---
+++ Assets/Scripts/Background/ScrollingBackground.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Special thanks to Root Games for scrolling background explanation from Unity 2D: Scrolling Background.
+// [Video]. Youtube. Retrieved January 23, 2023 from https://www.youtube.com/watch?v=Wz3nbQPYwss
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float speed;
+
+    [SerializeField]
+    private Renderer bgRenderer;
+     
+    void Update()
+    {
+        // Time.timeScale added to stop background from moving when screen is paused
+        bgRenderer.material.mainTextureOffset += new Vector2(0, speed * Time.deltaTime * Time.timeScale);
+    }
+}
