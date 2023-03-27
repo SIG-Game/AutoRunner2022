6b2c67cbffae259f76929ef8d6e3109241697115
maiuryn <sebastianho0311@gmail.com>
2023-03-26 22:40:37 -0500

    Update Level End Trigger

--- Assets/Scripts/Game Controllers/LevelEndTriggerController.cs
+++ Assets/Scripts/Game Controllers/LevelEndTriggerController.cs
@@ -5,7 +5,7 @@ public class LevelEndTriggerController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {   
-        if(collision.tag == "Player")
+        if(collision.CompareTag("Player"))
         {
             SceneManager.LoadScene("LevelSelect");
         }
