1a25a8d8b2ba1f5a4787f027568986b72c1c2737
Isaac Sanchez <isanch35@uic.edu>
2023-05-24 12:56:22 -0500

    Addressed 3rd set of review comments

--- Assets/Scripts/Background/BeyondBorderBackgroundResizer.cs
+++ Assets/Scripts/Background/BeyondBorderBackgroundResizer.cs
@@ -12,32 +12,29 @@ using UnityEngine;
 
 public class BeyondBorderBackgroundResizer : MonoBehaviour
 {
-    private float sizeFactorWidth;
-    private float sizeFactorHeight;
-    private float borderWidth;
     private float borderEdge;
-    private float cameraEdge;
-    private float localXScale;
     private Vector2 currentCameraSize;
     private SpriteRenderer beyondBorderBgSprite;
 
-    public SpriteRenderer border;  // variable to store border sprite
-    public enum Side {Left, Right}; // Right if filling the space for the right side of the screen, Left otherwise
-    public Side side;
+    [SerializeField] private SpriteRenderer borderSprite;
+    [SerializeField] private enum Side {Left, Right}; // Right if filling the space for the right side of the screen, Left otherwise
+    [SerializeField] private Side side;
 
 
-    void Awake()
+    private void Awake()
     {
         float cameraHeight = Camera.main.orthographicSize * 2;
         currentCameraSize = new Vector2(cameraHeight * Camera.main.aspect, cameraHeight);
-        localXScale = transform.localScale.x;
-        borderWidth = border.size.x * border.transform.localScale.x;
+        float borderWidth = borderSprite.size.x * borderSprite.transform.localScale.x;
         beyondBorderBgSprite = GetComponent<SpriteRenderer>();
 
-        if (side == Side.Left) {
-            borderEdge = border.transform.position.x - (borderWidth / 2);
-        } else {
-            borderEdge = border.transform.position.x + (borderWidth / 2);
+        if (side == Side.Left) 
+        {
+            borderEdge = borderSprite.transform.position.x - (borderWidth / 2);
+        } 
+        else 
+        {
+            borderEdge = borderSprite.transform.position.x + (borderWidth / 2);
         }
 
         UpdateBackgroundFillSize();
@@ -48,31 +45,37 @@ public class BeyondBorderBackgroundResizer : MonoBehaviour
     // This function updates the empty right/left space.
     private void UpdateBackgroundFillSize()
     {
-        if (side == Side.Left) {
+        float cameraEdge;
+
+        if (side == Side.Left) 
+        {
             cameraEdge = Camera.main.transform.position.x - (currentCameraSize.x / 2);
-        } else {
+        } 
+        else 
+        {
             cameraEdge = Camera.main.transform.position.x + (currentCameraSize.x / 2);
         }
 
-        // If width of the selected border is partially/fully outside of the camera, no need to resize sprite.
-        if (((side == Side.Right) && borderEdge < cameraEdge) || ((side == Side.Left) && borderEdge > cameraEdge)) {
+        // If width of borderSprite is partially/fully outside of the camera, no need to resize sprite.
+        if ((side == Side.Right && borderEdge < cameraEdge) || (side == Side.Left && borderEdge > cameraEdge)) 
+        {
             // The sizing factor is calculated, with the sprite renderer transformed to fill an empty side of the screen.
-            sizeFactorWidth = Mathf.Abs(borderEdge - cameraEdge);
-
-            sizeFactorHeight = currentCameraSize.y;
-            transform.position = new Vector2((borderEdge + (cameraEdge)) / 2, Camera.main.transform.position.y);
-            beyondBorderBgSprite.size = new Vector2(sizeFactorWidth / localXScale, sizeFactorHeight / localXScale);
+            float bgFillWidth = Mathf.Abs(borderEdge - cameraEdge);
+            float bgFillHeight = currentCameraSize.y;
+            transform.position = new Vector2((borderEdge + cameraEdge) / 2, Camera.main.transform.position.y);
+            beyondBorderBgSprite.size = new Vector2(bgFillWidth / transform.localScale.x, bgFillHeight / transform.localScale.y);
         }
     }
-    
 
-    void Update()
+
+    private void Update()
     {
         Vector2 newCameraSize;
         float cameraHeight = Camera.main.orthographicSize * 2;
         newCameraSize = new Vector2(cameraHeight * Camera.main.aspect, cameraHeight);
         
-        if (currentCameraSize != newCameraSize) {            
+        if (currentCameraSize != newCameraSize) 
+        {            
             currentCameraSize = newCameraSize;
             UpdateBackgroundFillSize();
         }
