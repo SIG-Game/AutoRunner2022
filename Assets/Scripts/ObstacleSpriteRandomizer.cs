593410f395134c4fb35f0e5e346afc29ccf2e516
Joshua J. Williams <116985621+joshjwilli@users.noreply.github.com>
2023-08-17 20:40:05 -0500

    Changed Script Name

--- Assets/Scripts/ObstacleSpriteRandomizer.cs
+++ Assets/Scripts/ObstacleSpriteRandomizer.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ObstacleRandomSprite : MonoBehaviour
+public class ObstacleSpriteRandomizer : MonoBehaviour
 {
     [SerializeField]
     private Sprite[] sprites;
